3b1820d401400fd745365d2bede67cd8d0160572
Nazareno Montesoro <n.montesoro@gmail.com>
2018-07-23 16:16:25 -0300

    Actualizado para incluir path de xls

--- BatchTest.bat
+++ BatchTest.bat
@@ -1,4 +1,4 @@
 @echo off
 
 FOR /L %%i IN (1,1,6) DO (
-	ExportSICOSS_t.exe .\Recibos.dbc %%i .\SICOSS-%%i.txt)
+	ExportSICOSS_t.exe .\Recibos.dbc %%i .\SICOSS-%%i.txt e:\_Temp\RecibosMC\ExportSICOSS\ExportSICOSS\Recibo.xls)
