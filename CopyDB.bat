91131e6325864daf137b06a7aa3e34e14949f3a2
Nazareno Montesoro <n.montesoro@gmail.com>
2018-07-24 14:35:26 -0300

    Cambios en espaciado

--- CopyDB.bat
+++ CopyDB.bat
@@ -22,7 +22,10 @@ IF NOT EXIST %PATHEXPORT%\ MKDIR %PATHEXPORT%
 copy %PATHSICOSS%\SIJP12.mdb sijp12\
 copy %PATHSICOSS%\SI220000.mdb sijp12\
 
+echo.
 echo Ya puede cerrar SIAP.
+echo.
+echo.
 SET /P MONTH=Que mes desea calcular? (1-12):
 
 ExportSICOSS.exe %PATHEMPRESA%\Recibos.dbc %MONTH% %PATHEXPORT%\SICOSS-%MONTH%.txt %PATHEMPRESA%\Recibo.xls
