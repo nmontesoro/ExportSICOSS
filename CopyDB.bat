1e883e439497b5cf419817420c22d9708768548a
Nazareno Montesoro <n.montesoro@gmail.com>
2018-07-24 13:45:49 -0300

    Parametrizado para llamar con varias empresas

--- CopyDB.bat
+++ CopyDB.bat
@@ -1,7 +1,9 @@
 @echo off
 
-SET SIAP=e:\SIAP_MejC\siap.exe
-SET PATHSICOSS=e:\SIAP_MejC\sijp12
+SET SIAP=%1
+SET PATHEMPRESA=%2
+SET PATHSICOSS=%3
+SET PATHEXPORT=%4
 
 echo Abra el SICOSS y luego siga las instrucciones. Presione enter para continuar (se abrira SIAP)...
 pause > nul
@@ -11,13 +13,13 @@ start %SIAP%
 echo Si ya abrio SICOSS, presione enter...
 pause > nul
 
-IF NOT EXIST sijp12\ mkdir sijp12
+IF NOT EXIST %PATHEMPRESA%\sijp12\ mkdir %PATHEMPRESA%\sijp12
 
 copy %PATHSICOSS%\SIJP12.mdb sijp12\
 
 copy %PATHSICOSS%\SI220000.mdb sijp12\
 
 echo Ya puede cerrar SIAP.
-SET /P sMonth=Que mes desea calcular? (1-12):
+SET /P MONTH=Que mes desea calcular? (1-12):
 
-ExportSICOSS.exe .\Recibos.dbc %sMonth% .\SICOSS-%sMonth%.txt e:\_Temp\RecibosMC\ExportSICOSS\ExportSICOSS\Recibo.xls
+%PATHEMPRESA%\ExportSICOSS.exe %PATHEMPRESA%\Recibos.dbc %MONTH% %PATHEXPORT%\SICOSS-%MONTH%.txt %PATHEMPRESA%\Recibo.xls
